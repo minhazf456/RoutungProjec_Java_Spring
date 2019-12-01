1e7fc1ed933f924b9fb536de5ea89c4acf1d1bb5
minhazf456 <minhazf@gmail.com>
2019-12-01 14:48:38 -0800

    Completed routingproject

---
+++ src/main/java/com/codingdojo/routingproject/RoutingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.routingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingprojectApplication.class, args);
+	}
+
+}
